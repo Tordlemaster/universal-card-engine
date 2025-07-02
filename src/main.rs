61cc56a23d5a1a39adb5d8f3ebfc47347a51d681
Tordlemaster <thaliaballas@outlook.com>
2025-07-02 00:06:39 -0400

    Beginning implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+pub mod rules;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/rules/variable.rs
@@ -0,0 +1,46 @@
+use std::collections::HashMap;
+
+#[derive(Clone)]
+pub struct VarBindSet {
+    int_contents: HashMap<String, usize>,
+    str_contents: HashMap<String, String>
+}
+
+impl VarBindSet {
+    pub fn new () -> VarBindSet {
+        VarBindSet {
+            int_contents: HashMap::new(),
+            str_contents: HashMap::new()
+        }
+    }
+
+    ///Insert a new int variable into the VarBindSet
+    pub fn insert_int_var (&mut self, name: String, value: usize) {
+        self.int_contents.insert(name, value);
+    }
+
+    ///Insert a new string variable into the VarBindSet
+    pub fn insert_str_var (&mut self, name: String, value: String) {
+        self.str_contents.insert(name, value);
+    }
+
+    ///Remove an existing int variable
+    pub fn remove_int_var (&mut self, name: String) {
+        self.int_contents.remove(&name);
+    }
+
+    ///Remove an existing str variable
+    pub fn remove_str_var (&mut self, name: String) {
+        self.str_contents.remove(&name);
+    }
+
+    ///Look up the value of an existing int variable
+    pub fn get_int_val (&mut self, name: String) -> Option<&usize> {
+        self.int_contents.get(&name)
+    }
+
+    //Look up the value of an existing variable
+    pub fn get_str_val (&mut self, name: String) -> Option<&String> {
+        self.str_contents.get(&name)
+    }
+}
