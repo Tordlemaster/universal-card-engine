c4372e08fb30d4dcd0fcd3b62d1bfe4cff6f7082
Tordlemaster <peterballas@outlook.com>
2025-07-04 00:38:13 -0400

    Refactored mod routine

--- src/rules/routine/iter_routine.rs
+++ src/rules/routine/iter_routine.rs
@@ -1,38 +1,4 @@
-use crate::rules::{game::GameWorld, variable::VarBindSet};
-
-pub trait Routine {
-    fn execute (&self, bindings: &VarBindSet, game_world: &mut GameWorld) -> ();
-
-    fn undo (&self, bindings: &VarBindSet, game_world: &mut GameWorld) -> ();
-}
-
-pub struct BaseRoutine {
-    routine: Vec<Box<dyn Routine>>,
-    bindings: VarBindSet
-}
-
-impl BaseRoutine {
-    pub fn new (routine: Vec<Box<dyn Routine>>, bindings: VarBindSet) -> BaseRoutine {
-        BaseRoutine {
-            routine: routine,
-            bindings: bindings
-        }
-    }
-}
-
-impl Routine for BaseRoutine {
-    fn execute (&self, bindings: &VarBindSet, game_world: &mut GameWorld) -> () {
-        for r in self.routine.iter() {
-            r.execute(&bindings.clone(), game_world);
-        }
-    }
-
-    fn undo (&self, bindings: &VarBindSet, game_world: &mut GameWorld) -> () {
-        for r in self.routine.iter().rev() {
-            r.undo(&bindings.clone(), game_world);
-        }
-    }
-}
+use crate::rules::{game::GameWorld, routine::routine::Routine, variable::VarBindSet};
 
 pub struct ForPlayerRoutine {
     routine: Vec<Box<dyn Routine>>
